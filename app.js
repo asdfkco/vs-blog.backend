c543c5736f92fda6cbec1a0a274785509aab4932
asdfkco <rlacksdhr0423@naver.com>
2022-11-10 14:01:29 +0900

    center_education

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const indexRouter = require("./router/index.js");
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
 
+
+app.use(cors());
+
 app.use("/",indexRouter);
-
-app.get("/",(req,res) => {
-    res.send("asasdfasfsafㅁㄴㅇㅁㅇㅁd");
-})
-
 // app.get("/asda", (req,res) => {
 //     res.status(404).send("오오옹")
 // })
